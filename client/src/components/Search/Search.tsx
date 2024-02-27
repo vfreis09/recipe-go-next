bee0710d7373e0a30092069846ffd0f988335e50
vfreis09 <reis.vicente27@gmail.com>
2024-02-27 10:52:52 -0300

    modal search

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -2,9 +2,19 @@ import styles from "./Search.module.css";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
+interface ModalState {
+  isOpen: boolean;
+}
+
 const SearchInput = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [modalState, setModalState] = useState<ModalState>({ isOpen: false });
+
   const router = useRouter();
+
+  const toggleModal = (): void => {
+    setModalState((prevModal) => ({ isOpen: !prevModal.isOpen }));
+  };
 
   const onSearch = (event: React.FormEvent) => {
     event.preventDefault();
@@ -14,14 +24,26 @@ const SearchInput = () => {
   };
 
   return (
-    <form onSubmit={onSearch}>
-      <input
-        placeholder="Search..."
-        className={styles.input}
-        value={searchQuery}
-        onChange={(event) => setSearchQuery(event.target.value)}
-      />
-    </form>
+    <div>
+      <button onClick={toggleModal}>Search</button>
+      {modalState.isOpen && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <form onSubmit={onSearch}>
+              <input
+                placeholder="Search..."
+                className={styles.input}
+                value={searchQuery}
+                onChange={(event) => setSearchQuery(event.target.value)}
+              />
+            </form>
+            <button className={styles.closeButton} onClick={toggleModal}>
+              &times;
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
