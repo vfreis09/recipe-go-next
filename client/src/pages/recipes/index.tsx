dcd7706b6ef1ce2414dba16fe888259096971755
vfreis09 <reis.vicente27@gmail.com>
2024-03-21 20:14:01 -0300

    add cards at recipes page

--- client/src/pages/recipes/index.tsx
+++ client/src/pages/recipes/index.tsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import type { RecipeID } from "@/types/types";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import styles from "@/styles/Recipes.module.css";
+import Card from "@/components/Card/Card";
 
-export default function RecipesPage() {
+// Fetch data from database
+export const getServerSideProps = (async () => {
+  try {
+    const res = await fetch("http://localhost:4000/api/recipes");
+    const data = await res.json();
+    return { props: { data } };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return { props: { data: [] } }; // Return an empty array if fetching fails
+  }
+}) as GetServerSideProps<{ data: RecipeID[] }>;
+
+export default function RecipesPage({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [dataRecipes, setDataRecipes] = useState<RecipeID[]>(data || []);
+
+  const cards = dataRecipes.map((recipe: RecipeID) => {
+    return <Card key={recipe.id} {...recipe} />;
+  });
+
   return (
     <div className={styles.pageContainer}>
       <Header />
@@ -24,7 +48,7 @@ export default function RecipesPage() {
           <button className={styles.button}>baked-goods</button>
           <button className={styles.button}>dessert</button>
         </div>
-        <div>cards</div>
+        <div className={styles.cardContainer}>{cards}</div>
       </div>
       <Footer />
     </div>
