84e8758d1be6a562dde68591693b63b9fafad3e2
vfreis09 <reis.vicente27@gmail.com>
2024-03-22 20:16:12 -0300

    button background-color onclick update

--- client/src/pages/recipes/index.tsx
+++ client/src/pages/recipes/index.tsx
@@ -22,6 +22,16 @@ export default function RecipesPage({
   data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [dataRecipes, setDataRecipes] = useState<RecipeID[]>(data || []);
+  const [selectedType, setSelectedType] = useState<string | null>(null);
+
+  const handleTypeSelect = (type: string | null) => {
+    setSelectedType(type);
+  };
+
+  function handler() {
+    //filter function on the buttons
+    console.log("ello");
+  }
 
   const cards = dataRecipes.map((recipe: RecipeID) => {
     return <Card key={recipe.id} {...recipe} />;
@@ -39,14 +49,76 @@ export default function RecipesPage({
           </p>
         </div>
         <div>
-          <button className={styles.button}>all</button>
-          <button className={styles.button}>breakfast</button>
-          <button className={styles.button}>appetizer</button>
-          <button className={styles.button}>salad</button>
-          <button className={styles.button}>main-course</button>
-          <button className={styles.button}>side-dish</button>
-          <button className={styles.button}>baked-goods</button>
-          <button className={styles.button}>dessert</button>
+          <button
+            className={styles.button}
+            style={{ backgroundColor: selectedType === null ? "#9FDC26" : "" }}
+            onClick={() => handleTypeSelect(null)}
+          >
+            all
+          </button>
+          <button
+            className={styles.button}
+            style={{
+              backgroundColor: selectedType === "breakfast" ? "#9FDC26" : "",
+            }}
+            onClick={() => handleTypeSelect("breakfast")}
+          >
+            breakfast
+          </button>
+          <button
+            className={styles.button}
+            style={{
+              backgroundColor: selectedType === "appetizer" ? "#9FDC26" : "",
+            }}
+            onClick={() => handleTypeSelect("appetizer")}
+          >
+            appetizer
+          </button>
+          <button
+            className={styles.button}
+            style={{
+              backgroundColor: selectedType === "salad" ? "#9FDC26" : "",
+            }}
+            onClick={() => handleTypeSelect("salad")}
+          >
+            salad
+          </button>
+          <button
+            className={styles.button}
+            style={{
+              backgroundColor: selectedType === "main-course" ? "#9FDC26" : "",
+            }}
+            onClick={() => handleTypeSelect("main-course")}
+          >
+            main-course
+          </button>
+          <button
+            className={styles.button}
+            style={{
+              backgroundColor: selectedType === "side-dish" ? "#9FDC26" : "",
+            }}
+            onClick={() => handleTypeSelect("side-dish")}
+          >
+            side-dish
+          </button>
+          <button
+            className={styles.button}
+            style={{
+              backgroundColor: selectedType === "baked-goods" ? "#9FDC26" : "",
+            }}
+            onClick={() => handleTypeSelect("baked-goods")}
+          >
+            baked-goods
+          </button>
+          <button
+            className={styles.button}
+            style={{
+              backgroundColor: selectedType === "dessert" ? "#9FDC26" : "",
+            }}
+            onClick={() => handleTypeSelect("dessert")}
+          >
+            dessert
+          </button>
         </div>
         <div className={styles.cardContainer}>{cards}</div>
       </div>
